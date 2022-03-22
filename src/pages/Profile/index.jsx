4c479f9a4e8d4b692fb893f1e442df801a2c66a2
Chrishan.Duttoo <chrisduttoo@gmail.com>
2022-03-22 10:56:11 +0100

    front|| first init

---
+++ src/pages/Profile/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Test = styled.div`
+  color: #5843e4;
+  font-size: 22px;
+  font-weight: bold;
+`;
+
+function App() {
+  return <Test className="App">TEST</Test>;
+}
+
+export default App;
